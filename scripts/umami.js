c584bcf89a9ce0ace03cac15b86c8bd1741df7cc
mdguggenbichler <michael@mdg-labs.dev>
2026-03-31 11:04:24 +0000

    refactor: encapsulate script creation in an IIFE and change insertion point to head for improved loading performance

--- scripts/umami.js
+++ scripts/umami.js
@@ -1,5 +1,6 @@
-var script = document.createElement('script');
-script.defer = true;
-script.src = 'https://umami.mdg-labs.dev/script.js';
-script.setAttribute('data-website-id', '597a0be4-2a12-42c4-b926-fbd339fbf35f');
-document.body.appendChild(script);
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://umami.mdg-labs.dev/script.js';
+    script.setAttribute('data-website-id', '597a0be4-2a12-42c4-b926-fbd339fbf35f');
+    document.head.appendChild(script);
+  })();
