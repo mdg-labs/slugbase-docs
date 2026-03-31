7250da2ffcdcf85fdc79941217bbab8e894cd313
mdguggenbichler <michael@mdg-labs.dev>
2026-03-31 10:59:02 +0000

    fix: change script insertion from head to body for better loading performance

--- scripts/umami.js
+++ scripts/umami.js
@@ -2,4 +2,4 @@ var script = document.createElement('script');
 script.defer = true;
 script.src = 'https://umami.mdg-labs.dev/script.js';
 script.setAttribute('data-website-id', '597a0be4-2a12-42c4-b926-fbd339fbf35f');
-document.head.appendChild(script);
+document.body.appendChild(script);
