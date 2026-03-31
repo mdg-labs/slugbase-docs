03bc16b0e0297b685f14ca8cfd37c8ed09b619f0
mdguggenbichler <michael@mdg-labs.dev>
2026-03-31 11:06:21 +0000

    fix: restore script insertion to head for improved loading performance

--- scripts/umami.js
+++ scripts/umami.js
@@ -1,6 +1,7 @@
+console.log('umami.js loaded');
 (function() {
-    var script = document.createElement('script');
-    script.src = 'https://umami.mdg-labs.dev/script.js';
-    script.setAttribute('data-website-id', '597a0be4-2a12-42c4-b926-fbd339fbf35f');
-    document.head.appendChild(script);
-  })();
+  var script = document.createElement('script');
+  script.src = 'https://umami.mdg-labs.dev/script.js';
+  script.setAttribute('data-website-id', '597a0be4-2a12-42c4-b926-fbd339fbf35f');
+  document.head.appendChild(script);
+})();
