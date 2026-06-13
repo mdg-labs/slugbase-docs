ef47c3e232722f67505aa7b75af5e57606666eb2
mdguggenbichler <michael@mdg-labs.dev>
2026-06-14 00:22:35 +0200

    prune

--- scripts/umami-analytics.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var script = document.createElement('script');
-  script.src = 'https://umami.mdg-labs.dev/script.js';
-  script.setAttribute('data-website-id', 'dfde8624-08cd-42bc-9732-8abb6a227c64');
-  script.setAttribute('data-do-not-track', 'true');
-  document.head.appendChild(script);
-})();
